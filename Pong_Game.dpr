2cd552d561d5f3fe5d5a9a10f826d55bd26fe353
Krystian <mikoda.krystian@gmail.com>
2024-10-09 00:16:47 +0200

    Initial commit

---
+++ Pong_Game.dpr
@@ -0,0 +1,14 @@
+program Pong_Game;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  main in 'main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
